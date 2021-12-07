1a411af5834b0b7ec1f94ab5966e55163ab2fc38
Nimesh Maharjan <maharjannimesh11@gmail.com>
2021-12-07 14:19:42 +0545

    CRUD done

--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,6 +1,7 @@
 package com.spring.orm.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -10,12 +11,36 @@ import com.spring.orm.entities.Student;
 
 public class StudentDao {
 	private HibernateTemplate hibernateTemplate;
+	private List<Student> loadAll;
 	
 	//save student
 	@Transactional
 	public int insert(Student student) {
 		Integer i = (Integer) this.hibernateTemplate.save(student);
 		return i;
+	}
+
+	//get the single data(object)
+	public Student getStudent(int studentId){
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+	//get all students(all rows)
+	public List<Student> getAllStudents(){
+		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
+
+	@Transactional
+	public void deleteStudent(int studentId){
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+	}
+
+	//updating data
+	@Transactional
+	public void updateStudent(Student student){
+		this.hibernateTemplate.update(student);
 	}
 
 	public HibernateTemplate getHibernateTemplate() {
